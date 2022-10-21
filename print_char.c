e897ff22f409cf394dce9ea4a65fd19aafc2b9e7
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-10-21 14:45:22 -0700

    final draft

---
+++ print_char.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_char - prints a char
+ *@list: list
+ *
+ *Return: 1
+ */
+
+int print_char(va_list list)
+
+{
+
+	int aux;
+
+	aux = va_arg(list, int);
+
+	_putchar(aux);
+
+	return (1);
+}
+
+
