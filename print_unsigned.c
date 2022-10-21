9de667fa56464a3cbbb4dcec9fa45f3aabd61e31
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-10-21 16:25:27 -0700

    fix unsigned

--- print_unsigned.c
+++ print_unsigned.c
@@ -10,7 +10,7 @@ int print_unsigned(va_list list)
 {
 	char *p_buff;
 	int size;
-	
+
 	p_buff = itoa(va_arg(list, unsigned int), 10);
 	size = print((p_buff != NULL) ? p_buff : "NULL");
 	return (size);
