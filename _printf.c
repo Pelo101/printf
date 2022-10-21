56607ffce7d576e77daed9a021b7e76001e742ee
ShaquilleTL <labistours@gmail.com>
2022-10-21 14:18:30 -0700

    Added final draft

---
+++ _printf.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _printf - Produces outpust according to a format
+ * @format: is a character string. The format string
+ *
+ * Return: the number of charactes printed, null byte excluded
+ */
+int _printf(const char *format, ...)
+{
+	int size;
+	va_list args;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	size = _strlen(format);
+	if (size <= 0)
+	
+	{
+		return (0);
+	}
+
+	va_start(args, format);
+	size = handler(format, args);
+
+	_putchar(-1);
+	va_end(args);
+
+	return (size);
+}
